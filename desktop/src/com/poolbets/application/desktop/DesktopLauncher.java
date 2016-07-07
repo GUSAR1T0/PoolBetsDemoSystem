84b292cbcf0f6e72d095b998a4ee8ddd7eafc588
GUSARITO <vl_vasilevsky@mail.ru>
2016-07-07 11:36:38 +0300

    Initial commit

---
+++ desktop/src/com/poolbets/application/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.poolbets.application.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.poolbets.application.PoolBetsApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 480;
+		config.height = 854;
+		new LwjglApplication(new PoolBetsApp(), config);
+	}
+}
